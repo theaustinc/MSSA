da43c75cca7ea5fd8060a445de0f7c3f270e0706
theaustinc <107366949+theaustinc@users.noreply.github.com>
2022-06-13 20:55:23 -0400

    Add files via upload

--- Presentation_SCRIPT.ps1
+++ Presentation_SCRIPT.ps1
@@ -5,12 +5,35 @@ The script then goes on to link to the tenant, select a subscription,
     build a new resourcegroup, and finally build the virtual machine.
 The last few line include some basic cmdlets to teardown what the script built,
     for testing and avoiding charges.
+
+It would be neat to be able to run without admin priveleges. 
+    Microsoft.PowerShell.SecretManagement?
+    Microsoft.PowerShell.SecretStore?
 #>
 
+
+
+$isadmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if ($isadmin -eq $false){
+    write-host "Must be admin to continue"
+    break
+}
+else {
+
+
+
 if(!(get-installedmodule az -ErrorAction SilentlyContinue)){
-    write-host "The correct module is not installed. Installing now..."
-    ## add prompt the user if wants to install or not? 
-    install-module az -force  
+    write-host "The necessary module is not installed. Would you like to install it now? (Y/N)"
+    $res = Read-host
+    if($res -eq "Y"){
+        write-host "installing module now..."
+        install-module az -force
+        }    
+    else {
+        write-host "Ending the script..."
+        break
+        }
     } 
 else {
     write-host "The module is already installed"
@@ -32,6 +55,7 @@ else {
 }
 
 # Building a new Resource group
+# But what if they want to use an existing RG?
 write-host "Please specify a name for a new Resource Group..."
 $newRG = Read-host
 write-host "Please specify a location..."
@@ -39,18 +63,21 @@ $RGloc = Read-host
 new-azresourcegroup -name $newRG -location $RGloc
 
 # Building a new VM
+# only works with ubuntults right now. why?
 $rg = get-azresourcegroup | Where-Object {$_.ResourceGroupName -eq $newRG} | select-object *
 Write-Host "Please specify a name for your new Virtual Machine..."
 $vm = Read-host
-Write-Host "Which Operating System would you like to use?"
+Write-Host "Which Operating System would you like to use? [type UBUNTULTS i havent tried it with others yet]"
 $OS = Read-host
-new-azvm -name $vm -ResourceGroupName $rg.ResourceGroupName `
- -Location $RGloc -Image $OS -Credential (Get-credential) 
+new-azvm -name $vm -ResourceGroupName $rg.ResourceGroupName -Location $RGloc -Image $OS -Credential (Get-credential) 
+
+}
 #
 
 <#
 TEARDOWN
 remove-azvm -name $vm -ResourceGroupName $newRG -force
 remove-azresourcegroup -name $newRG -force
+Disconnect-AzAccount
 uninstall-module az
 #>
